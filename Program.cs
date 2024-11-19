86248bf6ee2d6ad688c5cafb1b7cce3ae40873e9
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-11-18 20:32:57 -0500

    Revert "Commit before testing the restore function"
    This reverts commit 7d0d5fd4022ae2b9580419b8783c962a1b9ab568.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,1 @@
-﻿class Program
-{
-    public int RemoveDuplicates(int[] nums)
-    {
-        return 4;
-    }
-
-    static void Main(string[] args)
-    {
-        Program p = new Program();
-        int[] nums1 =  [1,1,2];
-        p.RemoveDuplicates(nums1);
-    }
-}
+﻿
