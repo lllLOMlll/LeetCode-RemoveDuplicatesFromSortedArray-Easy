544026db4752360d691b595c473042a7a0945aeb
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-11-22 07:18:57 -0500

    Last commit

--- Program.cs
+++ Program.cs
@@ -2,20 +2,19 @@
 {
     public int RemoveDuplicates(int[] nums)
     {
-        int numsLength = nums.Length;
-        List<int> duplicates = new List<int>();
-        int uniqueElement = 0;
+        if (nums.Length == 0) return 0;
 
-        for (int i = 0; i < nums.Length; i++)
+        int indexToWrite = 1;
+
+        for (int i = 1; i < nums.Length; i++)
         {
-            if (!duplicates.Contains(nums[i]))
+            if (nums[i] != nums[i - 1])
             {
-                duplicates.Add(nums[i]);
-                uniqueElement += 1;
+                nums[indexToWrite] = nums[i];
+                indexToWrite += 1;
             }
         }
-
-        return uniqueElement;
+        return indexToWrite;
     }
 
     static void Main(string[] args)
@@ -24,8 +23,8 @@
         int[] nums1 = [1, 1, 2];
         Console.WriteLine("2 -> " + p.RemoveDuplicates(nums1));
 
-        int[] nums2 = [0,0,1,1,1,2,2,3,3,4];
-        Console.WriteLine("4 -> " + p.RemoveDuplicates(nums2)); 
+        int[] nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+        Console.WriteLine("5 -> " + p.RemoveDuplicates(nums2));
     }
 }
 
